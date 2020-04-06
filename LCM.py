754bc0d23c3f837d7bd1ebccd98c47c5255ac7cc
madhurinanavare <58253685+madhurinanavare@users.noreply.github.com>
2020-04-06 17:58:40 +0530

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -1,22 +1,18 @@
 # Python Program to find the L.C.M. of two input number
 
-def compute_lcm(x, y):
+x = 54
+y = 24
 
-   # choose the greater number
-   if x > y:
-       greater = x
-   else:
-       greater = y
+if x > y:
+    greater = x
+else:
+    greater = y
 
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
+while(True):
+    
+    if((greater % x == 0) and (greater % y == 0)):
+        lcm = greater
+        break
+    greater += 1
 
-   return lcm
-
-num1 = 54
-num2 = 24
-
-print("The L.C.M. is", compute_lcm(num1, num2))
+print("The L.C.M. is", lcm)
